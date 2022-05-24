bb1acfd3666ad7d9fcc3cb8a55fa8e4afa1e129e
NahidPc <nahidhasan141400@gmail.com>
2022-05-24 21:20:26 +0600

    add login router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const ejs = require('ejs');
 const path = require('path');
+require('dotenv').config();
 
 // set port 
 const PORT = process.env.PORT || 3300;
@@ -21,9 +22,24 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 
 // set route 
-app.get('/',(req,res)=>{
-    res.render("home");
-})
+// set home route 
+    app.get('/',(req,res)=>{
+        res.render("home");
+    });
+    app.get('/login',(req,res)=>{
+        res.render("login");
+    });
+// set login route 
+    app.post('/login',(req,res)=>{
+        let {username , password} = req.body;
+        console.log(process.env.PASSWORD);
+        if(username === process.env.USER_NAME && password === process.env.PASSWORD){
+            res.send(`<h1> wellcome ${username} you login sucssesfully</h1>`)
+        }else{
+            res.send('wrong password')
+        }
+        
+    })
 
 // init server 
 app.listen(PORT , () => {
